dedd776662f775cb2e2cd2d8bcaa891d66a3e6d2
selvanayaki678 <selvanayaki849@gmail.com>
2023-12-12 15:43:31 +0530

    Added the RDS creation code

---
+++ RDS/main.tf
@@ -0,0 +1,30 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_db_instance" "my-sql" {
+  allocated_storage    = 5
+  db_name              = "mysql-db"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "admin@123"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  publicly_accessible = true
+  storage_type ="standard"
+  
+
+}

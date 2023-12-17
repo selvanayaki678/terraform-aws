38e3f8c0d8613549cc071e6d53461bcad79a15fd
selvanayaki678 <67256407+selvanayaki678@users.noreply.github.com>
2023-12-17 14:57:27 +0530

    Added the subnet, vpc details

--- RDS/main.tf
+++ RDS/main.tf
@@ -10,24 +10,53 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
+
+resource "aws_vpc" "main" {
+  cidr_block = "192.168.20.0/24"
+  tags = {
+    Name = "rds-vpc"
+  }
+}
+resource "aws_subnet" "rds-s1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.20.0/25"
+
+  tags = {
+    Name = "rds-subnet1"
+  }
+}
+resource "aws_subnet" "rds-s2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.20.127/25"
+
+  tags = {
+    Name = "rds-subnet2"
+  }
+}
+resource "aws_db_subnet_group" "default" {
+  name       = "rds-main"
+  subnet_ids = [aws_subnet.rds-s1, aws_subnet.rds-s2.id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
 resource "aws_db_instance" "my-sql" {
   allocated_storage    = 5
-  db_name              = "mysqdbcrud"
+  db_name              = "mysql-db"
   engine               = "mysql"
   engine_version       = "8.0"
-  identifier           = "mysql-crud-rds"
+  db_subnet_group_name  = aws_db_subnet_group.default.id
   instance_class       = "db.t3.micro"
   username             = "admin"
-  password             = "admin123"
+  password             = "admin@123"
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
   publicly_accessible = true
   storage_type ="standard"
-  tags = {
-  name = "rds-mysql-crud"
-}
+  
 
 }
